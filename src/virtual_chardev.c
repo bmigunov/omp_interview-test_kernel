f2866b233d311de3ecc22588ed2fa31242b12411
Bogdan Migunov <bogdanmigunov@yandex.ru>
2019-04-01 19:57:06 +0500

    Got rid of comments, moved definitions to header.

--- src/virtual_chardev.c
+++ src/virtual_chardev.c
@@ -1,39 +1,11 @@
-/*
- *  chardev.c: Создает символьное устройство, доступное только для чтения.
- *  Возвращает сообщение с указанием количества произведённых попыток чтения из
- *  файла устройства.
- */
-
-
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <asm/uaccess.h>  /* Определение функции put_user */
-
-#define SUCCESS 0
-#define BUF_LEN 80            /* Максимальная длина сообщения */
-#define DEVICE_NAME "chardev" /* Имя устройства, см. /proc/devices */
+#include "virtual_chardev.h"
 
 
 
 
-/*
- *  Прототипы функций, обычно их выносят в заголовочный файл (.h)
- */
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
-
-
-/*
- *  Глобальные переменные, объявлены как static, воизбежание конфликтов имен.
- */
-static int  Major;           /* Старший номер устройства нашего драйвера */
-static int  Device_Open = 0; /* Устройство открыто? */
-static char msg[BUF_LEN];    /* Здесь будет собираться текст сообщения */
+static int  Major;
+static int  Device_Open = 0;
+static char msg[BUF_LEN];
 static char *msg_Ptr;
 
 static struct file_operations fops =
@@ -45,9 +17,6 @@ static struct file_operations fops =
 };
 
 
-/*
- *  Функции
- */
 int init_module(void)
 {
     Major = register_chrdev(0, DEVICE_NAME, &fops);
@@ -69,21 +38,10 @@ int init_module(void)
 
 void cleanup_module(void)
 {
-    /*
-     *  Отключение устройства
-     */
     unregister_chrdev(Major, DEVICE_NAME);
 }
 
 
-/*
- *  Обработчики
- */
-
-/*
- *  Вызывается, когда процесс пытается открыть файл устройства, например,
- *  командой "cat /dev/chardev".
- */
 static int device_open(struct inode *inode, struct file *file)
 {
     static int counter = 0;
@@ -98,69 +56,34 @@ static int device_open(struct inode *inode, struct file *file)
     return SUCCESS;
 }
 
-/*
- *  Вызывается, когда процесс закрывает файл устройства.
- */
 static int device_release(struct inode *inode, struct file *file)
 {
-    Device_Open--; /* Теперь мы готовы обслужить другой процесс */
+    Device_Open--;
 
-    /*
-     *  Уменьшить счетчик обращений, иначе, после первой же удачной попытки
-     *  открыть файл устройства, вы никогда не сможете выгрузить модуль.
-     */
     module_put(THIS_MODULE);
 
     return 0;
 }
 
-/*
- *  Вызывается, когда процесс пытается прочитать уже открытый файл устройства.
- */
-static ssize_t device_read(struct file *filp, /* см. include/linux/fs.h */
-                           char *buffer,      /* буфер */
-                           size_t length,     /* размер буфера */
+static ssize_t device_read(struct file *filp, char *buffer, size_t length,
                            loff_t * offset)
 {
-    /*
-     *  Количество байт, фактически записанных в буфер
-     */
     int bytes_read = 0;
 
-    /*
-     *  Если достигли конца сообщения, вернуть 0, как признак конца файла.
-     */
     if(*msg_Ptr == 0)
         return 0;
 
-    /*
-     *  Перемещение данных в буфер
-     */
     while(length && *msg_Ptr)
     {
-      /*
-       *  Буфер находится в пространстве пользователя (в сегменте данных), а не
-       *  в пространстве ядра, поэтому простое присваивание здесь недопустимо.
-       *  Для того, чтобы скопировать данные, мы используем функцию put_user(),
-       *  которая перенесёт данные из пространства ядра в пространство
-       *  пользователя.
-       */
         put_user(*(msg_Ptr++), buffer++);
 
         length--;
         bytes_read++;
     }
 
-    /*
-     *  В большинстве своем, функции чтения возвращают количество байт, записанных в буфер.
-     */
     return bytes_read;
 }
 
-/*
- *  Вызывается, когда процесс пытается записать в устройство, например, так:
- *  echo "hi" > /dev/chardev
- */
 static ssize_t device_write(struct file *filp, const char *buff, size_t len,
                             loff_t *off)
 {
