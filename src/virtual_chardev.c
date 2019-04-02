e3ccb65f5131dc46479efdbb09b26706223e71c1
Bogdan Migunov <bogdanmigunov@yandex.ru>
2019-04-02 05:04:04 +0500

    Renamed functions, added necessary macros, printks

--- src/virtual_chardev.c
+++ src/virtual_chardev.c
@@ -3,34 +3,68 @@
 
 
 
-static struct file_operations fops =
+static unsigned int cbuf_size = DEFAULT_CBUF_SIZE;
+module_param(cbuf_size, uint, S_IRUGO);
+
+static struct file_operations vchardev_fops =
 {
+    .owner =   THIS_MODULE,
+    .open =    vchardev_open,
+    .release = vchardev_release,
+    .read =    vchardev_read,
+    .write =   vchardev_write,
 };
 
 
-int init_module(void)
+static int __init vchardev_initialize(void)
 {
+    printk(KERN_WARNING "Initializing virtual character device module...\n");
+    printk(KERN_DEBUG "Circular buffer size parameter: %d\n", cbuf_size);
+
+    return 0;
 }
 
-void cleanup_module(void)
+static void __exit vchardev_cleanup(void)
 {
+    printk(KERN_WARNING "Cleaning everything up and shutting down...\n");
 }
 
 
-static int device_open(struct inode *inode, struct file *file)
+static int vchardev_open(struct inode *inode, struct file *filp)
 {
+    printk(KERN_DEBUG "Someone is trying to open the device file.\n");
+
+    return 0;
 }
 
-static int device_release(struct inode *inode, struct file *file)
+static int vchardev_release(struct inode *inode, struct file *filp)
 {
+    printk(KERN_DEBUG "Someone is trying to close the device file.\n");
+
+    return 0;
 }
 
-static ssize_t device_read(struct file *filp, char *buffer, size_t length,
-                           loff_t * offset)
+static ssize_t vchardev_read(struct file *filp, char *buffer, size_t len,
+                             loff_t *offset)
 {
+    printk(KERN_DEBUG "Someone is trying to read from the device file.\n");
+
+    return 0;
 }
 
-static ssize_t device_write(struct file *filp, const char *buff, size_t len,
-                            loff_t *off)
+static ssize_t vchardev_write(struct file *filp, const char *buffer, size_t len,
+                              loff_t *offset)
 {
+    printk(KERN_DEBUG "Someone is trying to write to the device file.\n");
+
+    return 0;
 }
+
+
+module_init(vchardev_initialize);
+module_exit(vchardev_cleanup);
+
+MODULE_LICENSE(VCHARDEV_LICENSE);
+MODULE_AUTHOR(VCHARDEV_AUTHOR);
+MODULE_DESCRIPTION(VCHARDEV_DESC);
+MODULE_VERSION(VCHARDEV_VERSION);
--- src/virtual_chardev.h
+++ src/virtual_chardev.h
@@ -1,12 +1,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/sched.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
+#define VCHARDEV_LICENSE "Dual BSD/GPL"
+#define VCHARDEV_AUTHOR "Bogdan Migunov <bogdanmigunov@yandex.ru>"
+#define VCHARDEV_DESC "Virtual character device driver."
+#define VCHARDEV_VERSION "1.0"
+
+#define DEFAULT_CBUF_SIZE 512
 
 
 
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+static int vchardev_open(struct inode *, struct file *);
+static int vchardev_release(struct inode *, struct file *);
+static ssize_t vchardev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t vchardev_write(struct file *, const char *, size_t, loff_t *);
